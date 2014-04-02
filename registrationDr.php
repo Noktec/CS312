7dc489e8ee72adab3b709f02da0218c0f011378f
noktec <noktec@noktec.com>
2014-04-02 20:50:27 +0100

    Update Registration Process Dr
    Update Registration Process Dr

--- registrationDr.php
+++ registrationDr.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 	<link rel="stylesheet" type="text/css" href="CSS/registration.css" media="all" />
+    <script type="text/javascript" SRC="JS/verifpassword.js"></script> 
 
 </head>
 <body>
@@ -14,23 +15,30 @@
 					<!-- place header here --> 
             </header>       
       <div  class="registration">
-    		<form id="RegistrationForm" method="post" action="">
+    		<form name="registrationForm" id="registrationForm" method="post" action="registrationProcessDr.php">
     			<p class="contact"><label for="name">First Name</label></p> 
     			<input id="name" name="name" placeholder="First Name" required="" tabindex="1" type="text"> 
 
-    			<p class="contact"><label for="FamilyName">Last Name</label></p> 
-    			<input id="FamilyName" name="FamilyName" placeholder="Last Name" required="" tabindex="2" type="text"> 
+    			<p class="contact"><label for="familyName">Last Name</label></p> 
+    			<input id="familyName" name="familyName" placeholder="Last Name" required="" tabindex="2" type="text"> 
 
-    			<p class="contact"><label for="Speciality">Speciality</label></p> 
-    			<input id="Speciality" name="Speciality" placeholder="Speciality" required="" tabindex="3" type="text"> 
+    			<p class="contact"><label for="speciality">Speciality</label></p> 
+    			<input id="speciality" name="speciality" placeholder="Speciality" required="" tabindex="3" type="text"> 
             
-                <p class="contact"><label for="phone">Mobile phone</label></p> 
+                <p class="contact"><label for="phone">Phone</label></p> 
                 <input id="phone" name="phone" placeholder="phone number" required="" type="text"> <br>
+
+                <p class="contact"><label for="email">Email</label></p> 
+                <input id="email" name="email" placeholder="example@domain.com" required="" type="email"> 
 
                 <p class="contact"><label for="password">Create a password</label></p> 
                 <input type="password" id="password" name="password" required=""> 
+
                 <p class="contact"><label for="repassword">Confirm your password</label></p> 
-                <input type="password" id="repassword" name="repassword" required=""> 
+                <input type="password" id="repassword" name="repassword" required="" oninput="check(this)"> 
+
+                <span id="passwordValidity" class="passwordValidity"></br></span>
+
             
             <input class="buttom" name="submit" id="submit" tabindex="5" value="Sign me up" type="submit"> 	 
    		</form> 
---
+++ registrationProcessDr.php
@@ -0,0 +1,42 @@
+<?php
+include_once "core/connect.php";
+
+//I do not salt the password though 
+//but in real life i'd use bcrypt not sha512.
+function hashPassword($password){
+$hash = hash('sha512', $password);
+return $hash;
+}
+
+if (isset($_POST['submit'])) {
+
+
+        $stmt = $connexion->prepare('INSERT INTO doctors(
+            Name, 
+            Surname, 
+            Speciality,
+            PhoneNumber,  
+            Password,
+            Email)
+        VALUES(
+            :Name, 
+            :Surname, 
+            :Speciality, 
+            :PhoneNumber, 
+            :Password,
+            :Email)');
+
+        //we hash the password first.
+        $password = hashPassword($_POST['password']);
+
+        $stmt->bindParam(':Name',       $_POST['name'],         PDO::PARAM_STR);
+        $stmt->bindParam(':Surname',    $_POST['familyName'],   PDO::PARAM_STR);
+        $stmt->bindParam(':Speciality', $_POST['speciality'],   PDO::PARAM_STR);
+        $stmt->bindParam(':PhoneNumber',$_POST['phone'],        PDO::PARAM_STR);
+        $stmt->bindParam(':Password',   $password,              PDO::PARAM_STR);
+        $stmt->bindParam(':Email',      $_POST['email'],        PDO::PARAM_STR);
+        $stmt->execute();
+        print_r($stmt->errorInfo()); 
+
+}
+?>
